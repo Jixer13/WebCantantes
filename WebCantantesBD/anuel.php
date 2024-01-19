3d6229fbf139d25a6c00bc8273e5fb7f9bcb3ec5
DaudeAA <92319889+AimanJr970@users.noreply.github.com>
2024-01-19 09:05:50 +0100

    Add files via upload

---
+++ WebCantantesBD/anuel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="prcss.css">
+    <title>Anuel</title>
+</head>
+<body>
+    <h1>Anuel AA</h1><br>
+
+    <div class="artist-info">
+        <p>Anuel AA, cuyo nombre real es Emmanuel Gazmey Santiago, es un cantante y compositor puertorriqueño de música urbana. Nació el 27 de noviembre de 1992 en Carolina, Puerto Rico. Anuel se destacó en la escena musical con su estilo único y colaboraciones con otros artistas prominentes.</p>
+    </div>
+
+    <div class="video-container">
+        <!-- Inserta el código de inserción del video de YouTube para Anuel AA -->
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/T6uJ-2Lknr0" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+</body>
+</html>
---
+++ WebCantantesBD/puertorico.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="prcss.css">
+    <title>PR</title>
+</head>
+<body>
+
+    <h1>Selección de Artistas Puertorriqueños</h1><br>
+
+    <div class="image-container">
+        <div class="artista">
+            <a href="anuel.php"><img src="anuelfoto.jpg" alt="Anuel AA"></a>
+            <p>Anuel AA</p>
+        </div>
+        
+        <div class="artista">
+            <a href="bryant.php"><img src="bryantfoto.jpg" alt="Bryant Mayers"></a>
+            <p>Bryant Mayers</p>
+        </div>
+
+        <div class="artista">
+            <a href="bb.php"><img src="bbfoto.jpg" alt="Bad Bunny"></a>
+            <p>Bad Bunny</p>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <div class="artista">
+            <a href="myke.php"><img src="mykefoto.jpg" alt="Myke Towers"></a>
+            <p>Myke Towers</p>
+        </div>
+
+        <div class="artista">
+            <a href="jhayco.php"><img src="jhaycofoto.jpg" alt="Jhay Cortez"></a>
+            <p>Jhay Cortez</p>
+        </div>
+
+        <div class="artista">
+            <a href="yovng.php"><img src="yovngfoto.jpg" alt="Yovngchimi"></a>
+            <p>Yovngchimi</p>
+        </div>
+    </div>
+
+</body>
+</html
