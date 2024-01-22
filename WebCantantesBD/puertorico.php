0a7e6607feb74ffa20784942f365d690a7731244
DaudeAA <92319889+AimanJr970@users.noreply.github.com>
2024-01-22 09:18:20 +0100

    Update puertorico.php

--- WebCantantesBD/puertorico.php
+++ WebCantantesBD/puertorico.php
@@ -11,34 +11,20 @@
 
     <div class="image-container">
         <div class="artista">
-            <a href="anuel.php"><img src="anuelfoto.jpg" alt="Anuel AA"></a>
+            <a href="anuel.php"><img src="https://i1.sndcdn.com/artworks-000209165842-374bii-t500x500.jpg" alt="Anuel AA" 
+            width="100" height="200"></a></a>
             <p>Anuel AA</p>
         </div>
         
         <div class="artista">
-            <a href="bryant.php"><img src="bryantfoto.jpg" alt="Bryant Mayers"></a>
+            <a href="bryant.php"><img src="https://yt3.googleusercontent.com/TrOhnr_gMc_KmwYhKJ12Z7JP_LNLcy2w7ABvcN0gcnD23sCtEKiZ5xkLCRlmGzHzu-rTf_zHHQ=s900-c-k-c0x00ffffff-no-rj" alt="Bryant Mayers" 
+            width="100" height="200">
+            </a>
             <p>Bryant Mayers</p>
         </div>
-
         <div class="artista">
-            <a href="bb.php"><img src="bbfoto.jpg" alt="Bad Bunny"></a>
-            <p>Bad Bunny</p>
-        </div>
-    </div>
-
-    <div class="image-container">
-        <div class="artista">
-            <a href="myke.php"><img src="mykefoto.jpg" alt="Myke Towers"></a>
-            <p>Myke Towers</p>
-        </div>
-
-        <div class="artista">
-            <a href="jhayco.php"><img src="jhaycofoto.jpg" alt="Jhay Cortez"></a>
-            <p>Jhay Cortez</p>
-        </div>
-
-        <div class="artista">
-            <a href="yovng.php"><img src="yovngfoto.jpg" alt="Yovngchimi"></a>
+            <a href="yovng.php"><img src="https://i.pinimg.com/736x/2a/ef/07/2aef071aff3c7763896766227a191414.jpg" alt="Yovngchimi" 
+            width="100" height="200"></a>
             <p>Yovngchimi</p>
         </div>
     </div>
