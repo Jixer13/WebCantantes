f6ecbd684b2038a10c5652d55b2744e86deaad7d
Jixer13 <98114337+Jixer13@users.noreply.github.com>
2024-01-29 11:52:15 +0100

    Update registro.php

--- WebCantantesBD/registro.php
+++ WebCantantesBD/registro.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="registrocss.css">
+    <link rel="stylesheet" href="regcss.css">
     <title>Registro</title>
 </head>
 <body>
@@ -10,20 +10,21 @@
         <div>
             <h2>Registro</h2>
             
-            <h3>Nombre de Usuario</h3>
+            <h3> Usuario</h3>
             <input type="text" name="username" placeholder="Nombre de Usuario" required>
 
-            <h3>Correo</h3>
+            <h3> Correo</h3>
             <input type="email" name="gmail" placeholder="Correo" required>
 
-            <h3>Contraseña</h3>
+            <h3> Contraseña</h3>
             <input type="password" name="password" placeholder="Contraseña" required>
-
+            <h5>Si ya tienes cuenta, puedes iniciar sesión <a href="login.php">aquí</a></h5>
             <input type="submit" value="Registrarse" class="Redirect">
         </div>
     </form>
 </body>
 </html>
+
 
 
 
